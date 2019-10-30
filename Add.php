b52b212dbaabfbb42640f2034c02a832948fe294
Aytach <47595400+Aytach@users.noreply.github.com>
2019-10-30 22:34:16 +0400

    Update Add.php
    Добавлена защита от отправки POST запроса со стороннего сайта и защита от XSS атаки (Межсайтовый скриптинг).

--- Add.php
+++ Add.php
@@ -1,25 +1,30 @@
 <?php
     if (isset($_POST['add']))
     {
-    	$name =$_POST['name'];
-    	$price = $_POST['price'];
-    	$description = $_POST['description'];
-    	$email = $_POST['email'];
-    	$leadtime = $_POST['leadtime'];
+        if($_POST['security'] == md5('AddAdvertisement.php'.'Add.php')) 
+        {
+            $name = htmlspecialchars($_POST['name']);
+            $price = htmlspecialchars($_POST['price']);
+            $description = htmlspecialchars($_POST['description']);
+            $email = htmlspecialchars($_POST['email']);
+            $leadtime = htmlspecialchars($_POST['leadtime']);
 
-		$mysqli = new mysqli('localhost', 'root', '', 'volgolife');
-		    if($mysqli->connect_errno)
-		    {
-		      echo 'Ошибка в подключении к базе данных ('.$mysqli->connect_errno().'): '.$mysqli->connect_error();
-		      exit();
-		    }
+            $mysqli = new mysqli('localhost', 'root', '', 'volgolife');
+            if($mysqli->connect_errno)
+            {
+              echo 'Ошибка в подключении к базе данных ('.$mysqli->connect_errno().'): '.$mysqli->connect_error();
+              exit();
+            }
 
-    	 $result = $mysqli->query( "INSERT INTO `advertisements` (`id`, `name`, `price`, `description`, `leadtime`, `email`) VALUES (NULL, '$name', '$price', '$description', '$leadtime', '$email')");
+            $result = $mysqli->query( "INSERT INTO `advertisements` ( `name`, `price`, `description`, `leadtime`, `email`) VALUES ( '$name', '$price', '$description', '$leadtime', '$email')");
 
-    	if($result == 'true')        
-    		    header("Location: Stends.php" );
-    	else 
-    		echo "Ошибка при добавлении!";
+            if($result == 'true')        
+                header("Location: Stends.php" );
+            else {
+                echo "Ошибка при добавлении!";
+                header("Location: AddAdvertisement.php" );
+            }
+        } 
+        else die('Не туда попали!');   	   
     }
-
 ?> 
