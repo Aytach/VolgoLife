3e4905a7e6d966ea58a262f5fba538f2db5fea08
Aytach <47595400+Aytach@users.noreply.github.com>
2019-11-19 23:24:08 +0400

    Update Add.php

--- Add.php
+++ Add.php
@@ -1,25 +1,25 @@
 <?php
-    if (isset($_POST['add']))
-    {
-        if($_POST['security'] == md5('AddAdvertisement.php'.'Add.php')) 
-        {
-            $name = htmlspecialchars($_POST['name']);
-            $price = htmlspecialchars($_POST['price']);
-            $description = htmlspecialchars($_POST['description']);
-            $email = htmlspecialchars($_POST['email']);
-            $leadtime = htmlspecialchars($_POST['leadtime']);
-            
-            require_once 'db.php';
-            
-            $result = mysqli_query($link, "INSERT INTO `advertisements` ( `name`, `price`, `description`, `leadtime`, `email`) VALUES ( '$name', '$price', '$description', '$leadtime', '$email')");
 
-            if($result == 'true')        
-                header("Location: Stends.php" );
-            else {
-                echo "Ошибка при добавлении!";
-                header("Location: AddAdvertisement.php" );
-            }
-        } 
-        else die('Не правильный вход!');   	   
+session_start();
+
+require_once 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (isset($_POST['add'], $_POST['email'], $_POST['leadtime'], $_POST['description'], $_POST['price'], $_POST['name'])) {
+        $name = $_POST['name'];
+        $price = $_POST['price'];
+        $description = $_POST['description'];
+        $email = $_POST['email'];
+        $leadtime = $_POST['leadtime'];
+
+
+        $sql = 'INSERT INTO advertisements(name, price, description, leadtime, email) VALUES(:name, :price, :description, :leadtime, :email)';
+
+        $query = $pdo->prepare($sql); //выполняем команду
+
+        $query->execute(['name' => $name, 'price' => $price, 'description' => $description, 'leadtime' => $leadtime, 'email' => $email]); //при выполнении(execute) устанавливаем значения в бд
+
+        header("Location: Stends.php");
     }
+}
 ?> 
