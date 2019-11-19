acf1d495c95e0df9c071b8862bfb1ac9daba502d
Aytach <47595400+Aytach@users.noreply.github.com>
2019-11-19 23:45:44 +0400

    Create form_edit_orders.php

---
+++ form_edit_orders.php
@@ -0,0 +1,31 @@
+<?php
+  require_once 'db.php';
+
+  $id_editc = trim($_REQUEST['id']);
+
+  $sql = $pdo->prepare('SELECT * FROM `customers` WHERE `id_customer` = :id');
+  $sql->execute([':id' => $id_editc]);
+  $data = $sql->fetch(PDO::FETCH_OBJ);
+
+
+  echo '<form class="d brd" novalidate name="Form"  onsubmit="return ValidateForm()" action="update_orders_save.php"method="post" enctype="multipart/form-data">
+  <br>
+  <h4>Добавьте изменения:</h4>                  
+  <br>
+  <label for="assessment">Оценка:</label>
+  <br>
+  <input type="text" name="assessment" value="' . $data->assessment . '"  placeholder="Ваша оценка:">
+  <br><br>
+  <label for="reviev">Отзыв:</label>
+  <br>
+  <input type="text" name="review" value="' . $data->review . '"  placeholder="Ваш отзыв:">
+  <br><br>
+  <label for="myFile">Фото готового стенда:</label>
+  <br>
+  <img src="' . $data->img . '" alt="фото"/>
+  <input type="file" name="myFile" value="">
+  <br><br>
+  <input type="submit" name="edit" class="btn btn-success" value="Изменить">
+  <input type="hidden" name="id" id="id" value="' . $id_editc . '">
+  </form>';
+?>
