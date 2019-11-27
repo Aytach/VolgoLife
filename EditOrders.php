12ede47657e5c698a89455afab537bb9057df110
Aytach <47595400+Aytach@users.noreply.github.com>
2019-11-27 22:45:42 +0400

    Update EditOrders.php

--- EditOrders.php
+++ EditOrders.php
@@ -31,29 +31,7 @@
     <div class="container">
         <div class="well">
             <div >
-                <script type="text/javascript">
-                    function ValidateForm() {
-
-                        var x = document.forms["Form"]["assessment"].value;
-                        if (x == "")
-                        {
-                            alert("Необходимо ввести оценку!");
-                            return false;
-                        }
-                        var adr_pattern = /^([1-9]|10)$/;
-                        if (adr_pattern.test(x) == false)
-                        {
-                            alert("Вы ввели некорректую оценку! Введите оценку от 1 до 10.");
-                            return false;
-                        }
-                        var x = document.forms["Form"]["review"].value;
-                        if (x == "")
-                        {
-                            alert("Необходимо ввести отзыв!");
-                            return false;
-                        }
-                    }
-                </script>
+                <script type="text/javascript" src="js\editorders_validate.js"></script>
                 <?php
                   require 'form_edit_orders.php';
                 ?>
