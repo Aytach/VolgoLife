bf44d7b52b2fb2e641d8a8d6a7893e13de07a291
Aytach <47595400+Aytach@users.noreply.github.com>
2019-11-27 22:49:35 +0400

    Добавление объявления

--- AddAdvertisements.php
+++ AddAdvertisements.php
@@ -20,111 +20,13 @@
         <link href="/docs/4.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
         <link href="blog.css" rel="stylesheet">
-        </head>
-        <style type="text/css">
-            input,textarea
-            {
-                width: 600px;
-                margin: 10 10 10 0;
-            }
-            .brd 
-            {
-                border: 2px solid #2F4F4F; 
-                background: #F5F5F5; 
-                padding: 5px; 
-            }
-            .cont
-            {
-                text-align: center;
-                margin-left: 25%; 
-                margin-top: 3%; 
-                width: 700px;
-            }
-            .z
-            {
-                border: 1px solid green;
-                margin-left: 515px;
-                margin-top: 25px;
-            }
-        </style>
+        <link href="css\addadvertisements_style.css" rel="stylesheet">
+    </head>
     <div class="cont">
         <div class="well">
             <div >
-                <script type="text/javascript">
+                <script type="text/javascript"src="js\addadvertisements_validate.js"></script>
 
-                    function validateForm() {
-
-                        var x = document.forms["myForm"]["name"].value;
-                        if (x == "")
-                        {
-                            alert("Необходимо ввести название!");
-                            return false;
-                        }
-                        var adr_pattern = /\S/;
-                        if (adr_pattern.test(x) == false)
-                        {
-                            alert("Вы ввели некорректное название!");
-                            return false;
-                        }
-
-
-                        var x = document.forms["myForm"]["price"].value;
-                        if (x == "")
-                        {
-                            alert("Необходимо ввести стоимость выполненной работы!");
-                            return false;
-                        }
-                        var adr_pattern = /^\d+$/;
-                        if (adr_pattern.test(x) == false)
-                        {
-                            alert("Вы ввели некорректную цену!");
-                            return false;
-                        }
-
-
-                        var x = document.forms["myForm"]["description"].value;
-                        if (x == "")
-                        {
-                            alert("Необходимо ввести короткое описание товара!");
-                            return false;
-                        }
-                        var adr_pattern = /\S/;
-                        if (adr_pattern.test(x) == false)
-                        {
-                            alert("Вы ввели некорректное описание товара!");
-                            return false;
-                        }
-
-                        var x = document.forms["myForm"]["leadtime"].value;
-                        if (x == "")
-                        {
-                            alert("Необходимо ввести время выполнения заказа!");
-                            return false;
-                        }
-                        var adr_pattern = /([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?/;
-                        if (adr_pattern.test(x) == false)
-                        {
-                            alert("Вы ввели некорректное время!");
-                            return false;
-                        }
-
-
-                        var x = document.forms["myForm"]["email"].value;
-                        if (x == "")
-                        {
-                            alert("Необходимо ввести E-mail!");
-                            return false;
-                        }
-                        var adr_pattern = /[0-9a-z_-]+@[0-9a-z_-]+\.[a-z]{2,5}/i;
-                        if (adr_pattern.test(x) == false)
-                        {
-                            alert("Вы ввели E-mail неправильно!");
-                            return false;
-                        }
-
-                    }
-
-                </script>
                 <form class = "brd" novalidate name="myForm" action="Add.php" onsubmit="return validateForm()" method="post" >
                     <br>
                     <h4>Добавьте свое объявление!</h4>
